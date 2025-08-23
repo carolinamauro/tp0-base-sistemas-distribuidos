e689bfa82db928552b65257673cfbbf33a78796b
caro <carolinaluciamauro@gmail.com>
2025-08-23 13:01:56 -0300

    uso clase

--- generar-compose.py
+++ generar-compose.py
@@ -1,56 +1,56 @@
 import yaml
 import sys
 
-default_data = {
-    "name": "tp0",
-    "services": {
-        "server": {
-            "container_name": "server",
-            "image": "server:latest",
-            "entrypoint": "python3 /main.py",
-            "environment": ["PYTHONUNBUFFERED=1", "LOGGING_LEVEL=DEBUG"],
-            "networks": ["testing_net"],
-        },
-    },
-    "networks": {
-        "testing_net": {
-            "ipam": {
-                "driver": "default",
-                "config": [{"subnet": "172.25.125.0/24"}],
-            }
-        }
-    }
-}
-
-def to_yaml(file_name, compose_data):
-    with open(file_name, "w") as f:
-        dump = yaml.dump(compose_data, default_flow_style=False, sort_keys=False)
-        f.write(dump)
-
-
-def generar_compose(file_name, clients_amount):
-    data = default_data.copy()
-    clients = {}
+class DockerComposeYamlGenerator:
     
-    for i in range(clients_amount):
-        client_name = f"client{i+1}"
-        clients[client_name] = {
-            "container_name": client_name,
-            "image": "client:latest",
-            "entrypoint": "/client",
-            "environment": ["CLI_ID=" + str(i + 1), "CLI_LOG_LEVEL=DEBUG"],
-            "networks": ["testing_net"],
-            "depends_on": ["server"],
+    def __init__(self, clients_amount, file_name):
+        self.file_name = file_name
+        self.compose = {
+            "name": "tp0",
+            "services": {
+                "server": {
+                    "container_name": "server",
+                    "image": "server:latest",
+                    "entrypoint": "python3 /main.py",
+                    "environment": ["PYTHONUNBUFFERED=1", "LOGGING_LEVEL=DEBUG"],
+                    "networks": ["testing_net"],
+                },  
+            },
+            "networks": {
+                "testing_net": {
+                    "ipam": {
+                        "driver": "default",
+                        "config": [{"subnet": "172.25.125.0/24"}],
+                    },
+                },
+            },
         }
+        self.add_clients(clients_amount)
         
-    data["services"].update(clients)
-    to_yaml(file_name, data)
+        
+    def add_clients(self, clients_amount):
+    
+        for i in range(clients_amount):
+            client_name = f"client{i+1}"
+            self.compose["services"][client_name] = {
+                "container_name": client_name,
+                "image": "client:latest",
+                "entrypoint": "/client",
+                "environment": ["CLI_ID=" + str(i + 1), "CLI_LOG_LEVEL=DEBUG"],
+                "networks": ["testing_net"],
+                "depends_on": ["server"],
+        }
+            
+    def generate_yaml(self):
+        text = yaml.safe_dump(self.compose, sort_keys=False, default_flow_style=False)
+        with open(self.file_name, "w", encoding="utf-8") as f:
+            f.write(text)
 
-      
 if __name__ == "__main__":
     if len(sys.argv) > 2:
         file_name = sys.argv[1]
         clients = int(sys.argv[2])
-        generar_compose(file_name, clients)
+        composeGenerator = DockerComposeYamlGenerator(clients, file_name)
+        composeGenerator.generate_yaml()
       
    
