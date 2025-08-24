5bd356a0c144c184b2eb648aa9d25b75bed28f9c
caro <carolinaluciamauro@gmail.com>
2025-08-24 13:32:22 -0300

    agrego mensajes esperados

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -2,5 +2,13 @@
 
 message="Hola, Mundo!"
 
-rta=$(docker run --rm --network tp0_testing_net busybox sh -c "echo $message | nc server 12345")
-echo $rta
+server_response=$(docker run --rm --network tp0_testing_net busybox sh -c "echo $message | nc server 12345")
+
+if [ "$server_serponse" == "$message" ]; then
+    echo "action: test_echo_server | result: success"
+    exit 0
+else
+    echo "action: test_echo_server | result: fail"
+    exit 1
+fi
+
