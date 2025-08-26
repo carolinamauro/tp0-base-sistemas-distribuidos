52aec8b469e3eb7857f1a92201949d89ea1f9ab4
caro <carolinaluciamauro@gmail.com>
2025-08-25 23:12:55 -0300

    arreglo naming variable

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -8,7 +8,7 @@ message="Hola, Mundo!"
 
 server_response=$(docker run --rm --network tp0_testing_net busybox sh -c "echo $message | nc $SERVER_IP $SERVER_PORT")
 
-if [ "$server_serponse" = "$message" ]; then
+if [ "$server_response" = "$message" ]; then
     echo "action: test_echo_server | result: success"
 else
     echo "action: test_echo_server | result: fail"
