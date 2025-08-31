a77fa5347e7af51680de670156d945bdcbaa6575
caro <carolinaluciamauro@gmail.com>
2025-08-31 13:51:35 -0300

    fix

--- client/common/agency.go
+++ client/common/agency.go
@@ -58,15 +58,14 @@ func (c *Agency) createAgencySocket() error {
 	return nil
 }
 
-func (c *Client) handleSigtermSignal() {
-	log.Infof("action: SIGTERM signal received | result: in_progress | client_id: %v", c.config.ID)
-	// Close the connection if it is open
-	if c.conn != nil {
-		c.conn.Close()
-		log.Infof("action: SIGTERM signal received | result: success | client_id: %v", c.config.ID)
+
+func (c *Agency) handleSigtermSignal() {
+	log.Infof("action: SIGTERM signal received| result: in_progress | agency_id: %v", c.config.ID)
+	if c.transport.conn != nil {
+		c.transport.Close()
+		log.Infof("action: SIGTERM signal received| result: success | agency_id: %v", c.config.ID)
 	}
 }
-
 
 // StartAgencyLoop Send messages to the Agency until some time threshold is met
 func (c *Agency) StartAgencyLoop() {
