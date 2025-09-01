24b12c8ab8d14cc3873850449fe7f66f2d620d93
caro <carolinaluciamauro@gmail.com>
2025-08-31 22:13:50 -0300

    fix

--- generar-compose.py
+++ generar-compose.py
@@ -34,7 +34,7 @@ CLIENT_TEMPLATE = """\
     depends_on:
       - server
     volumes:
-      - ./.data/dataset/agency-{id}.csv:/agency.csv
+      - ./.data/agency-{id}.csv:/agency.csv
       - ./client/config.yaml:/config.yaml
 """
 
