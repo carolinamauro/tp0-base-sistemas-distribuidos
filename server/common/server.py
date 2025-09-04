b2788e153e8d37a91005c42c3e14081abbb9d6b6
caro <carolinaluciamauro@gmail.com>
2025-09-04 14:48:46 -0300

    fix

--- server/common/server.py
+++ server/common/server.py
@@ -87,10 +87,12 @@ class Server:
         """
         
         self.__close_server_socket()
-        logging.info(f'action: SIGTERM signal received | result: success | server socket')
+        logging.info('action: SIGTERM signal received | result: in_progress')
         for client_socket in self._active_client_connections:
             client_socket.close()
-            logging.info(f'action: SIGTERM signal received | result: success | client socket')
+            logging.info("action: SIGTERM signal received | result: success | client socket")
+        socket_addr = self._server_socket.getsockname()[0]
+        logging.info(f'action: SIGTERM signal received | result: success | server socket: {socket_addr}')
         
 
         
