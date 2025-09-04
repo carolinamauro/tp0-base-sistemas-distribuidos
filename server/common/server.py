66e5e23113af8faadffee2f38505be2e798e94f5
caro <carolinaluciamauro@gmail.com>
2025-09-04 13:48:47 -0300

    fix sigterm

--- server/common/server.py
+++ server/common/server.py
@@ -87,13 +87,12 @@ class Server:
         2. Closes all active client connections
         """
         
+        socket_addr = self._server_socket.getsockname()[0]
+        logging.info(f'action: SIGTERM signal received | result: success | server socket: {socket_addr}')
         self.__close_server_socket()
-        logging.info('action: SIGTERM signal received | result: in_progress')
         for client_socket in self._active_client_connections:
             client_socket.close()
             logging.info(f'action: SIGTERM signal received | result: success | client socket: {client_socket}')
-        socket_addr = self._server_socket.getsockname()[0]
-        logging.info(f'action: SIGTERM signal received | result: success | server socket: {socket_addr}')
         
 
         
